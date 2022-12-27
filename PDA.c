fdc36fc486bc9f0d6b60cc45d35e3bf1d03fc9b1
Voorapavani <119840598+Voorapavani@users.noreply.github.com>
2022-12-27 09:52:39 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,c=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         c=c+i;     }     if(c==n)     {         printf("PERFECT");     }     else if(c<n)     {         printf("DEFICIENT");     }     else if(c>n)     {         printf("ABUNDANT");     } }

---
+++ PDA.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        c=c+i;
+    }
+    if(c==n)
+    {
+        printf("PERFECT");
+    }
+    else if(c<n)
+    {
+        printf("DEFICIENT");
+    }
+    else if(c>n)
+    {
+        printf("ABUNDANT");
+    }
+}
